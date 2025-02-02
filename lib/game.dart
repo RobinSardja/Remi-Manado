42ea3e7d86a3a6203075756269fb438ef1b3ec78
RobinSardja <robinsardja@gmail.com>
2025-02-01 20:06:25 -0500

    Seperated rank from value for cards.

--- lib/game.dart
+++ lib/game.dart
@@ -15,58 +15,58 @@ class GameScreen extends StatefulWidget {
 class _GameScreenState extends State<GameScreen> {
   int cardsUsed = 13;
   final deck = [
-    PlayingCard("assets/club/cardClubs_A.png", 1, Suit.club),
-    PlayingCard("assets/club/cardClubs_2.png", 2, Suit.club),
-    PlayingCard("assets/club/cardClubs_3.png", 3, Suit.club),
-    PlayingCard("assets/club/cardClubs_4.png", 4, Suit.club),
-    PlayingCard("assets/club/cardClubs_5.png", 5, Suit.club),
-    PlayingCard("assets/club/cardClubs_6.png", 6, Suit.club),
-    PlayingCard("assets/club/cardClubs_7.png", 7, Suit.club),
-    PlayingCard("assets/club/cardClubs_8.png", 8, Suit.club),
-    PlayingCard("assets/club/cardClubs_9.png", 9, Suit.club),
-    PlayingCard("assets/club/cardClubs_10.png", 10, Suit.club),
-    PlayingCard("assets/club/cardClubs_J.png", 11, Suit.club),
-    PlayingCard("assets/club/cardClubs_Q.png", 12, Suit.club),
-    PlayingCard("assets/club/cardClubs_K.png", 13, Suit.club),
-    PlayingCard("assets/diamond/cardDiamonds_A.png", 1, Suit.diamond),
-    PlayingCard("assets/diamond/cardDiamonds_2.png", 2, Suit.diamond),
-    PlayingCard("assets/diamond/cardDiamonds_3.png", 3, Suit.diamond),
-    PlayingCard("assets/diamond/cardDiamonds_4.png", 4, Suit.diamond),
-    PlayingCard("assets/diamond/cardDiamonds_5.png", 5, Suit.diamond),
-    PlayingCard("assets/diamond/cardDiamonds_6.png", 6, Suit.diamond),
-    PlayingCard("assets/diamond/cardDiamonds_7.png", 7, Suit.diamond),
-    PlayingCard("assets/diamond/cardDiamonds_8.png", 8, Suit.diamond),
-    PlayingCard("assets/diamond/cardDiamonds_9.png", 9, Suit.diamond),
-    PlayingCard("assets/diamond/cardDiamonds_10.png", 10, Suit.diamond),
-    PlayingCard("assets/diamond/cardDiamonds_J.png", 11, Suit.diamond),
-    PlayingCard("assets/diamond/cardDiamonds_Q.png", 12, Suit.diamond),
-    PlayingCard("assets/diamond/cardDiamonds_K.png", 13, Suit.diamond),
-    PlayingCard("assets/heart/cardHearts_A.png", 1, Suit.heart),
-    PlayingCard("assets/heart/cardHearts_2.png", 2, Suit.heart),
-    PlayingCard("assets/heart/cardHearts_3.png", 3, Suit.heart),
-    PlayingCard("assets/heart/cardHearts_4.png", 4, Suit.heart),
-    PlayingCard("assets/heart/cardHearts_5.png", 5, Suit.heart),
-    PlayingCard("assets/heart/cardHearts_6.png", 6, Suit.heart),
-    PlayingCard("assets/heart/cardHearts_7.png", 7, Suit.heart),
-    PlayingCard("assets/heart/cardHearts_8.png", 8, Suit.heart),
-    PlayingCard("assets/heart/cardHearts_9.png", 9, Suit.heart),
-    PlayingCard("assets/heart/cardHearts_10.png", 10, Suit.heart),
-    PlayingCard("assets/heart/cardHearts_J.png", 11, Suit.heart),
-    PlayingCard("assets/heart/cardHearts_Q.png", 12, Suit.heart),
-    PlayingCard("assets/heart/cardHearts_K.png", 13, Suit.heart),
-    PlayingCard("assets/spade/cardSpades_A.png", 1, Suit.spade),
-    PlayingCard("assets/spade/cardSpades_2.png", 2, Suit.spade),
-    PlayingCard("assets/spade/cardSpades_3.png", 3, Suit.spade),
-    PlayingCard("assets/spade/cardSpades_4.png", 4, Suit.spade),
-    PlayingCard("assets/spade/cardSpades_5.png", 5, Suit.spade),
-    PlayingCard("assets/spade/cardSpades_6.png", 6, Suit.spade),
-    PlayingCard("assets/spade/cardSpades_7.png", 7, Suit.spade),
-    PlayingCard("assets/spade/cardSpades_8.png", 8, Suit.spade),
-    PlayingCard("assets/spade/cardSpades_9.png", 9, Suit.spade),
-    PlayingCard("assets/spade/cardSpades_10.png", 10, Suit.spade),
-    PlayingCard("assets/spade/cardSpades_J.png", 11, Suit.spade),
-    PlayingCard("assets/spade/cardSpades_Q.png", 12, Suit.spade),
-    PlayingCard("assets/spade/cardSpades_K.png", 13, Suit.spade),
+    PlayingCard("assets/club/cardClubs_2.png", Rank.two, Suit.club, 2),
+    PlayingCard("assets/club/cardClubs_3.png", Rank.three, Suit.club, 3),
+    PlayingCard("assets/club/cardClubs_4.png", Rank.four, Suit.club, 4),
+    PlayingCard("assets/club/cardClubs_5.png", Rank.five, Suit.club, 5),
+    PlayingCard("assets/club/cardClubs_6.png", Rank.six, Suit.club, 6),
+    PlayingCard("assets/club/cardClubs_7.png", Rank.seven, Suit.club, 7),
+    PlayingCard("assets/club/cardClubs_8.png", Rank.eight, Suit.club, 8),
+    PlayingCard("assets/club/cardClubs_9.png", Rank.nine, Suit.club, 9),
+    PlayingCard("assets/club/cardClubs_10.png", Rank.ten, Suit.club, 10),
+    PlayingCard("assets/club/cardClubs_J.png", Rank.jack, Suit.club, 10),
+    PlayingCard("assets/club/cardClubs_Q.png", Rank.queen, Suit.club, 10),
+    PlayingCard("assets/club/cardClubs_K.png", Rank.king, Suit.club, 10),
+    PlayingCard("assets/club/cardClubs_A.png", Rank.ace, Suit.club, 11),
+    PlayingCard("assets/diamond/cardDiamonds_2.png", Rank.two, Suit.diamond, 2),
+    PlayingCard("assets/diamond/cardDiamonds_3.png", Rank.three, Suit.diamond, 3),
+    PlayingCard("assets/diamond/cardDiamonds_4.png", Rank.four, Suit.diamond, 4),
+    PlayingCard("assets/diamond/cardDiamonds_5.png", Rank.five, Suit.diamond, 5),
+    PlayingCard("assets/diamond/cardDiamonds_6.png", Rank.six, Suit.diamond, 6),
+    PlayingCard("assets/diamond/cardDiamonds_7.png", Rank.seven, Suit.diamond, 7),
+    PlayingCard("assets/diamond/cardDiamonds_8.png", Rank.eight, Suit.diamond, 8),
+    PlayingCard("assets/diamond/cardDiamonds_9.png", Rank.nine, Suit.diamond, 9),
+    PlayingCard("assets/diamond/cardDiamonds_10.png", Rank.ten, Suit.diamond, 10),
+    PlayingCard("assets/diamond/cardDiamonds_J.png", Rank.jack, Suit.diamond, 10),
+    PlayingCard("assets/diamond/cardDiamonds_Q.png", Rank.queen, Suit.diamond, 10),
+    PlayingCard("assets/diamond/cardDiamonds_K.png", Rank.king, Suit.diamond, 10),
+    PlayingCard("assets/diamond/cardDiamonds_A.png", Rank.ace, Suit.diamond, 11),
+    PlayingCard("assets/heart/cardHearts_2.png", Rank.two, Suit.heart, 2),
+    PlayingCard("assets/heart/cardHearts_3.png", Rank.three, Suit.heart, 3),
+    PlayingCard("assets/heart/cardHearts_4.png", Rank.four, Suit.heart, 4),
+    PlayingCard("assets/heart/cardHearts_5.png", Rank.five, Suit.heart, 5),
+    PlayingCard("assets/heart/cardHearts_6.png", Rank.six, Suit.heart, 6),
+    PlayingCard("assets/heart/cardHearts_7.png", Rank.seven, Suit.heart, 7),
+    PlayingCard("assets/heart/cardHearts_8.png", Rank.eight, Suit.heart, 8),
+    PlayingCard("assets/heart/cardHearts_9.png", Rank.nine, Suit.heart, 9),
+    PlayingCard("assets/heart/cardHearts_10.png", Rank.ten, Suit.heart, 10),
+    PlayingCard("assets/heart/cardHearts_J.png", Rank.jack, Suit.heart, 10),
+    PlayingCard("assets/heart/cardHearts_Q.png", Rank.queen, Suit.heart, 10),
+    PlayingCard("assets/heart/cardHearts_K.png", Rank.king, Suit.heart, 10),
+    PlayingCard("assets/heart/cardHearts_A.png", Rank.ace, Suit.heart, 11),
+    PlayingCard("assets/spade/cardSpades_2.png", Rank.two, Suit.spade, 2),
+    PlayingCard("assets/spade/cardSpades_3.png", Rank.three, Suit.spade, 3),
+    PlayingCard("assets/spade/cardSpades_4.png", Rank.four, Suit.spade, 4),
+    PlayingCard("assets/spade/cardSpades_5.png", Rank.five, Suit.spade, 5),
+    PlayingCard("assets/spade/cardSpades_6.png", Rank.six, Suit.spade, 6),
+    PlayingCard("assets/spade/cardSpades_7.png", Rank.seven, Suit.spade, 7),
+    PlayingCard("assets/spade/cardSpades_8.png", Rank.eight, Suit.spade, 8),
+    PlayingCard("assets/spade/cardSpades_9.png", Rank.nine, Suit.spade, 9),
+    PlayingCard("assets/spade/cardSpades_10.png", Rank.ten, Suit.spade, 10),
+    PlayingCard("assets/spade/cardSpades_J.png", Rank.jack, Suit.spade, 10),
+    PlayingCard("assets/spade/cardSpades_Q.png", Rank.queen, Suit.spade, 10),
+    PlayingCard("assets/spade/cardSpades_K.png", Rank.king, Suit.spade, 10),
+    PlayingCard("assets/spade/cardSpades_A.png", Rank.ace, Suit.spade, 11),
   ];
   final deckSize = 52;
   late List<PlayingCard> hand;
@@ -95,7 +95,7 @@ class _GameScreenState extends State<GameScreen> {
   void resetDeck() {
     deck.shuffle( random );
     hand = deck.sublist(0, handSize);
-    hand.sort((a, b) => a.rank.compareTo(b.rank));
+    hand.sort((a, b) => a.rank.index.compareTo(b.rank.index));
     if( sortedBySuit ) hand.sort((a, b) => a.suit.index.compareTo(b.suit.index));
     validHand = isHandValid();
   }
@@ -115,7 +115,7 @@ class _GameScreenState extends State<GameScreen> {
     Map<Set<PlayingCard>, bool> threes = {}, fours = {};
     List<PlayingCard> temp = List.from(hand);
 
-    temp.sort((a, b) => a.rank.compareTo(b.rank));
+    temp.sort((a, b) => a.rank.index.compareTo(b.rank.index));
 
     // 3 set
     for( int i = 0; i < 11; i++ ) {
@@ -141,8 +141,8 @@ class _GameScreenState extends State<GameScreen> {
     for( int i = 0; i < 11; i++ ) {
       if( ( temp[i].suit == temp[i + 1].suit &&
             temp[i].suit == temp[i + 2].suit ) &&
-          ( temp[i].rank + 1 == temp[i + 1].rank &&
-            temp[i].rank + 2 == temp[i + 2].rank ) ) {
+          ( temp[i].rank.index + 1 == temp[i + 1].rank.index &&
+            temp[i].rank.index + 2 == temp[i + 2].rank.index ) ) {
         threes[ { temp[i], temp[i + 1], temp[i +2] } ] = true;
       }
     }
@@ -152,9 +152,9 @@ class _GameScreenState extends State<GameScreen> {
       if( ( temp[i].suit == temp[i + 1].suit &&
             temp[i].suit == temp[i + 2].suit &&
             temp[i].suit == temp[i + 3].suit ) &&
-          ( temp[i].rank + 1 == temp[i + 1].rank &&
-            temp[i].rank + 2 == temp[i + 2].rank &&
-            temp[i].rank + 3 == temp[i + 3].rank ) ) {
+          ( temp[i].rank.index + 1 == temp[i + 1].rank.index &&
+            temp[i].rank.index + 2 == temp[i + 2].rank.index &&
+            temp[i].rank.index + 3 == temp[i + 3].rank.index ) ) {
         fours[ { temp[i], temp[i + 1], temp[i +2], temp[i + 3] } ] = true;
       }
     }
@@ -199,7 +199,7 @@ class _GameScreenState extends State<GameScreen> {
     random = Random(seed);
     deck.shuffle( random );
     hand = deck.sublist(0, handSize);
-    hand.sort((a, b) => a.rank.compareTo(b.rank));
+    hand.sort((a, b) => a.rank.index.compareTo(b.rank.index));
     validHand = isHandValid();
     
     super.initState();
@@ -281,7 +281,7 @@ class _GameScreenState extends State<GameScreen> {
                         hand.add( deck[cardsUsed] );
                         cardsUsed++;
                       }
-                      hand.sort((a, b) => a.rank.compareTo(b.rank));
+                      hand.sort((a, b) => a.rank.index.compareTo(b.rank.index));
                       if( sortedBySuit ) hand.sort((a, b) => a.suit.index.compareTo(b.suit.index));
                       selected.clear();
                     });
@@ -295,7 +295,7 @@ class _GameScreenState extends State<GameScreen> {
                   if( validHand ) {
                     setState(() {
                       for( PlayingCard card in hand ) {
-                        points += card.rank;
+                        points += card.val;
                       }
                       if( points < threshold ) {
                         setState( () => resetGame() );
@@ -323,14 +323,14 @@ class _GameScreenState extends State<GameScreen> {
               ),
               TextButton(
                 onPressed: () => setState(() {
-                  hand.sort((a, b) => a.rank.compareTo(b.rank));
+                  hand.sort((a, b) => a.rank.index.compareTo(b.rank.index));
                   sortedBySuit = false;
                 }),
                 child: Text( "Sort by rank" )
               ),
               TextButton(
                 onPressed: () => setState(() {
-                  hand.sort((a, b) => a.rank.compareTo(b.rank));
+                  hand.sort((a, b) => a.rank.index.compareTo(b.rank.index));
                   hand.sort((a, b) => a.suit.index.compareTo(b.suit.index));
                   sortedBySuit = true;
                 }),
@@ -344,19 +344,19 @@ class _GameScreenState extends State<GameScreen> {
               TextButton(
                 onPressed: () => setState(() {
                   hand = [
-                    PlayingCard("assets/club/cardClubs_A.png", 1, Suit.club),
-                    PlayingCard("assets/club/cardClubs_2.png", 2, Suit.club),
-                    PlayingCard("assets/club/cardClubs_3.png", 3, Suit.club),
-                    PlayingCard("assets/club/cardClubs_4.png", 4, Suit.club),
-                    PlayingCard("assets/club/cardClubs_5.png", 5, Suit.club),
-                    PlayingCard("assets/club/cardClubs_6.png", 6, Suit.club),
-                    PlayingCard("assets/club/cardClubs_7.png", 7, Suit.club),
-                    PlayingCard("assets/club/cardClubs_8.png", 8, Suit.club),
-                    PlayingCard("assets/club/cardClubs_9.png", 9, Suit.club),
-                    PlayingCard("assets/club/cardClubs_10.png", 10, Suit.club),
-                    PlayingCard("assets/club/cardClubs_J.png", 11, Suit.club),
-                    PlayingCard("assets/club/cardClubs_Q.png", 12, Suit.club),
-                    PlayingCard("assets/club/cardClubs_K.png", 13, Suit.club)
+                    PlayingCard("assets/club/cardClubs_2.png", Rank.two, Suit.club, 2),
+                    PlayingCard("assets/club/cardClubs_3.png", Rank.three, Suit.club, 3),
+                    PlayingCard("assets/club/cardClubs_4.png", Rank.four, Suit.club, 4),
+                    PlayingCard("assets/club/cardClubs_5.png", Rank.five, Suit.club, 5),
+                    PlayingCard("assets/club/cardClubs_6.png", Rank.six, Suit.club, 6),
+                    PlayingCard("assets/club/cardClubs_7.png", Rank.seven, Suit.club, 7),
+                    PlayingCard("assets/club/cardClubs_8.png", Rank.eight, Suit.club, 8),
+                    PlayingCard("assets/club/cardClubs_9.png", Rank.nine, Suit.club, 9),
+                    PlayingCard("assets/club/cardClubs_10.png", Rank.ten, Suit.club, 10),
+                    PlayingCard("assets/club/cardClubs_J.png", Rank.jack, Suit.club, 10),
+                    PlayingCard("assets/club/cardClubs_Q.png", Rank.queen, Suit.club, 10),
+                    PlayingCard("assets/club/cardClubs_K.png", Rank.king, Suit.club, 10),
+                    PlayingCard("assets/club/cardClubs_A.png", Rank.ace, Suit.club, 11),
                   ];
                   validHand = isHandValid();
                 }),
@@ -365,19 +365,19 @@ class _GameScreenState extends State<GameScreen> {
               TextButton(
                 onPressed: () => setState(() {
                   hand = [
-                    PlayingCard("assets/club/cardClubs_10.png", 10, Suit.club),
-                    PlayingCard("assets/club/cardClubs_J.png", 11, Suit.club),
-                    PlayingCard("assets/club/cardClubs_Q.png", 12, Suit.club),
-                    PlayingCard("assets/club/cardClubs_K.png", 13, Suit.club),
-                    PlayingCard("assets/diamond/cardDiamonds_J.png", 11, Suit.diamond),
-                    PlayingCard("assets/diamond/cardDiamonds_Q.png", 12, Suit.diamond),
-                    PlayingCard("assets/diamond/cardDiamonds_K.png", 13, Suit.diamond),
-                    PlayingCard("assets/heart/cardHearts_J.png", 11, Suit.heart),
-                    PlayingCard("assets/heart/cardHearts_Q.png", 12, Suit.heart),
-                    PlayingCard("assets/heart/cardHearts_K.png", 13, Suit.heart),
-                    PlayingCard("assets/spade/cardSpades_J.png", 11, Suit.spade),
-                    PlayingCard("assets/spade/cardSpades_Q.png", 12, Suit.spade),
-                    PlayingCard("assets/spade/cardSpades_K.png", 13, Suit.spade),
+                    PlayingCard("assets/club/cardClubs_J.png", Rank.jack, Suit.club, 10),
+                    PlayingCard("assets/club/cardClubs_Q.png", Rank.queen, Suit.club, 10),
+                    PlayingCard("assets/club/cardClubs_K.png", Rank.king, Suit.club, 10),
+                    PlayingCard("assets/club/cardClubs_A.png", Rank.ace, Suit.club, 11),
+                    PlayingCard("assets/diamond/cardDiamonds_Q.png", Rank.queen, Suit.diamond, 10),
+                    PlayingCard("assets/diamond/cardDiamonds_K.png", Rank.king, Suit.diamond, 10),
+                    PlayingCard("assets/diamond/cardDiamonds_A.png", Rank.ace, Suit.diamond, 11),
+                    PlayingCard("assets/heart/cardHearts_Q.png", Rank.queen, Suit.heart, 10),
+                    PlayingCard("assets/heart/cardHearts_K.png", Rank.king, Suit.heart, 10),
+                    PlayingCard("assets/heart/cardHearts_A.png", Rank.ace, Suit.heart, 11),
+                    PlayingCard("assets/spade/cardSpades_Q.png", Rank.queen, Suit.spade, 10),
+                    PlayingCard("assets/spade/cardSpades_K.png", Rank.king, Suit.spade, 10),
+                    PlayingCard("assets/spade/cardSpades_A.png", Rank.ace, Suit.spade, 11)
                   ];
                   validHand = isHandValid();
                 }),
@@ -386,19 +386,19 @@ class _GameScreenState extends State<GameScreen> {
                TextButton(
                 onPressed: () => setState(() {
                   hand = [
-                    PlayingCard("assets/club/cardClubs_A.png", 1, Suit.club),
-                    PlayingCard("assets/club/cardClubs_2.png", 2, Suit.club),
-                    PlayingCard("assets/club/cardClubs_3.png", 3, Suit.club),
-                    PlayingCard("assets/club/cardClubs_4.png", 4, Suit.club),
-                    PlayingCard("assets/diamond/cardDiamonds_A.png", 1, Suit.diamond),
-                    PlayingCard("assets/diamond/cardDiamonds_2.png", 2, Suit.diamond),
-                    PlayingCard("assets/diamond/cardDiamonds_3.png", 3, Suit.diamond),
-                    PlayingCard("assets/heart/cardHearts_A.png", 1, Suit.heart),
-                    PlayingCard("assets/heart/cardHearts_2.png", 2, Suit.heart),
-                    PlayingCard("assets/heart/cardHearts_3.png", 3, Suit.heart),
-                    PlayingCard("assets/spade/cardSpades_A.png", 1, Suit.spade),
-                    PlayingCard("assets/spade/cardSpades_2.png", 2, Suit.spade),
-                    PlayingCard("assets/spade/cardSpades_3.png", 3, Suit.spade),
+                    PlayingCard("assets/club/cardClubs_2.png", Rank.two, Suit.club, 2),
+                    PlayingCard("assets/club/cardClubs_3.png", Rank.three, Suit.club, 3),
+                    PlayingCard("assets/club/cardClubs_4.png", Rank.four, Suit.club, 4),
+                    PlayingCard("assets/club/cardClubs_5.png", Rank.five, Suit.club, 5),
+                    PlayingCard("assets/diamond/cardDiamonds_2.png", Rank.two, Suit.diamond, 2),
+                    PlayingCard("assets/diamond/cardDiamonds_3.png", Rank.three, Suit.diamond, 3),
+                    PlayingCard("assets/diamond/cardDiamonds_4.png", Rank.four, Suit.diamond, 4),
+                    PlayingCard("assets/heart/cardHearts_2.png", Rank.two, Suit.heart, 2),
+                    PlayingCard("assets/heart/cardHearts_3.png", Rank.three, Suit.heart, 3),
+                    PlayingCard("assets/heart/cardHearts_4.png", Rank.four, Suit.heart, 4),
+                    PlayingCard("assets/spade/cardSpades_2.png", Rank.two, Suit.spade, 2),
+                    PlayingCard("assets/spade/cardSpades_3.png", Rank.three, Suit.spade, 3),
+                    PlayingCard("assets/spade/cardSpades_4.png", Rank.four, Suit.spade, 4),
                   ];
                   validHand = isHandValid();
                 }),
--- lib/objects/cards.dart
+++ lib/objects/cards.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 
+enum Rank { two, three, four, five, six, seven, eight, nine, ten, jack, queen, king, ace }
+
 enum Suit { club, diamond, heart, spade }
 
 @immutable
 class PlayingCard {
   final String face;
-  final int rank;
+  final Rank rank;
   final Suit suit;
+  final int val;
 
-  const PlayingCard( this.face, this.rank, this.suit );
+  const PlayingCard( this.face, this.rank, this.suit, this.val );
 
   @override
   toString() {
