efb6a29cce83e2220dfef3a6869eb2c773a31a73
RobinSardja <robinsardja@gmail.com>
2025-02-13 14:16:16 -0500

    Removed Firebase.

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,8 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-import 'package:remi/firebase_options.dart';
 import 'home.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform
-  );
+void main() {
   runApp(const MainApp());
 }
 
