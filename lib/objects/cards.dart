16cce493029b4114456de2f45693b6cce108d8cb
RobinSardja <robinsardja@gmail.com>
2025-02-17 12:50:45 -0500

    Added map conversion for PlayingCard.

--- lib/objects/cards.dart
+++ lib/objects/cards.dart
@@ -31,6 +31,14 @@ class PlayingCard {
 
   const PlayingCard( this.face, this.rank, this.suit );
 
+  factory PlayingCard.fromMap( Map<String, dynamic> data ) {
+    return PlayingCard( data["face"], data["rank"], data["suit"] );
+  }
+
+  Map<String, dynamic> toMap() {
+    return { "face": face, "rank": rank, "suit": suit };
+  }
+
   @override
   toString() {
     return "Card( $rank, $suit )";
